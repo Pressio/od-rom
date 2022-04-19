dcb6f8fe3290353e132aa02564024d13acd6c6b1
Francesco Rizzi <fnrizzi@sandia.gov>
2022-04-19 11:56:42 +0200

    update driver script

--- code/run.sh
+++ code/run.sh
@@ -7,16 +7,16 @@ export WDIR=$2
 export prob=$3
 export scenario=$4
 
-# fom needs to be run always
-python3 main_run_fom.py \
-	--wdir ${WDIR} --pdadir ${PDADIR} --problem py_problems.${prob} -s ${scenario}
-
-# the following are all executed AFTER fom data is generated
 # note that any of the following scripts runs something
 # only if it is present in the scenario.
 # For example, let's say your problem/scenario only specifies to
 # run standard pod galerkin, then "main_full_domain_sample_mesh.py" is a noop
-# So bottom line, leave them all here that it does not cost anything
+# So bottom line, leave them all here, it does not cost anything.
+
+# fom needs to be always first
+python3 main_run_fom.py \
+	--wdir ${WDIR} --pdadir ${PDADIR} --problem py_problems.${prob} -s ${scenario}
+
 
 ################################
 ## standard galerkin
@@ -41,7 +41,8 @@ python3 main_compute_sample_meshes_for_od_domain.py --wdir ${WDIR} --pdadir ${PD
 python3 main_compute_proj_errors_for_od_domain.py --wdir ${WDIR}
 
 # 3. run actual rom and process errors
-python3 main_pod_od_galerkin.py --wdir ${WDIR} --pdadir ${PDADIR}
-python3 main_pod_od_galerkin_gappy_real.py --wdir ${WDIR}
+python3 main_pod_od_galerkin.py		     --wdir ${WDIR} --pdadir ${PDADIR}
+python3 main_pod_od_galerkin_gappy_real.py   --wdir ${WDIR}
+python3 main_pod_od_galerkin_quad_real.py    --wdir ${WDIR}
 python3 main_pod_od_galerkin_gappy_masked.py --wdir ${WDIR}
-python3 main_compute_errors_for_od_runs.py --wdir ${WDIR}
+python3 main_compute_errors_for_od_runs.py   --wdir ${WDIR}
